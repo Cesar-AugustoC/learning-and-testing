169adf96ae1594653168a7e9c02b196be77b3bb2
Cesar Carvalho <carvalhocesar7823@gmail.com>
2026-03-18 18:11:36 -0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,23 @@
+# nome = input("Coloque seu nome: ")
+# print(f'Muito prazer, {nome}')
+
+# primeiro_numero = input('digite um numero: ')
+# segundo_numero = input('digite outro numero: ')
+# Resultado = primeiro_numero + segundo_numero
+# print(f'sua expressão resultou em: {Resultado}')
+
+# digite um numero: 10
+# digite outro numero: 8
+# sua expressão resultou em: 108
+# esqueci de precisa colocar o int, hehehe, então vamos codar o codigo certo
+
+# primeiro_numero = int(input('digite um numero: '))
+# segundo_numero = int(input('digite outro numero: '))
+# Resultado = primeiro_numero + segundo_numero
+# print(f'sua expressão resultou em: {Resultado}')
+
+# digite um numero: 10
+# digite outro numero: 8
+# sua expressão resultou em: 18
+
+# Agora sim retornou certo
